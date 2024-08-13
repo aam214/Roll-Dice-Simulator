e467c83aa714993b12a0a3459313d4a0b19e0bfb
Ariana <152944814+aam214@users.noreply.github.com>
2024-08-12 23:27:03 -0500

    Created a function of getDiceChange to get a new dice face with the switch and cases. Added new variable of diceChange to the diceResult function.

--- dice.js
+++ dice.js
@@ -1,9 +1,39 @@
 const buttonRoll = document.getElementById("button-roll");
 const diceElement = document.getElementById("dice");
 
+
+function diceResult(){
+const diceNumber = Math.floor(Math.random() * 6) + 1;
+  
+const diceChange =getDiceChange(diceNumber);
+}
+
+function getDiceChange(diceNumber){
+  switch(diceNumber){
+  case 1:
+     return "&#9856;";
+
+  case 2:
+     return "&#9857;";
+
+  case 3:
+      return "&#9858;";
+       
+    case 4:
+      return "&#9859;";
+         
+    case 5:
+      return "&#9860;";
+      
+    case 6:
+      return "&#9861;";
+}
+}
+
 buttonRoll.addEventListener("click", () => {
 diceElement.classList.add("roll-animation");
 setTimeout(() =>{
   diceElement.classList.remove("roll-animation");
-}, 1000);
+  diceResult();
+}, 2000);
 });
